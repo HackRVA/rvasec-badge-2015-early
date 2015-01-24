7dd936434edd558b0fb502f450830597054bcb91
Morgan Stuart <morgansstuart243@gmail.com>
2015-01-24 16:42:42 -0500

    Starting to Statify snake game

---
+++ src/Stages15/Snake.c
@@ -0,0 +1,16 @@
+#include "Snake.h"
+void* construct_snake15(struct BadgeState *b_state){
+
+}
+
+void* snake15(struct BadgeState *b_state){
+
+}
+
+void* snake15_onEnter(struct BadgeState *b_state){
+
+}
+
+void* snake15_onExit(struct BadgeState *b_state){
+
+}
---
+++ src/Stages15/Snake.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   Snake.h
+ * Author: morgan
+ *
+ * Created on January 24, 2015, 3:51 PM
+ */
+
+#ifndef SNAKE_H
+#define	SNAKE_H
+#include "../badge15.h"
+#include "nokia.h"
+
+#define STATE_NAME Snake
+
+DECLARE_BASE_STATE(STATE_NAME)
+
+#endif	/* SNAKE_H */
+
