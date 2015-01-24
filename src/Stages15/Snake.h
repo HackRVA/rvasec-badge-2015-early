fdbbe2033ce3a0324e71a424a900a9a7d7d166b9
Morgan Stuart <morgansstuart243@gmail.com>
2015-01-24 17:18:35 -0500

    New Snake state working
    Previous implementation ported over

--- src/Stages15/Snake.h
+++ src/Stages15/Snake.h
@@ -10,8 +10,30 @@
 #include "../badge15.h"
 #include "nokia.h"
 
-#define STATE_NAME Snake
+#define STATE_NAME snake
+#define SNAKE_RATE 6000
+#define SNAKE_START_SIZE 5
 
+struct CoOrd
+{
+    unsigned char x, y;
+};
+
+//movement vector
+struct Velocity
+{
+    unsigned char xV, yV;
+};
+
+//snake linked list
+struct SnakeSeg
+{
+    struct CoOrd location;
+    struct Velocity direction;
+    struct SnakeSeg *next;
+
+};
+struct SnakeSeg snake_ll[200];
 DECLARE_BASE_STATE(STATE_NAME)
 
 #endif	/* SNAKE_H */
